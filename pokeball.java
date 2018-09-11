76af621b39d6be0416ca72b87c371e8f027feccd
yourgirlfriend <cmsmith2067@gmail.com>
2018-09-11 08:20:15 -0700

    Add files via upload

---
+++ pokeball.java
@@ -0,0 +1,19 @@
+package pokeball;
+
+public class pokeball {
+
+	public static void main(String[] args) {
+		
+		System.out.println("   @@@@@@@@@@\r\n" + 
+				"  @@@@@@@@@@@@\r\n" + 
+				" @@@@@@@@@@@@@@\r\n" + 
+				"@@@@@@@@@@@@@@@@\r\n" + 
+				"|||||[““““]|||||\r\n" + 
+				"|||||[....]|||||\r\n" + 
+				"OOOOOOOOOOOOOOOO\r\n" + 
+				" OOOOOOOOOOOOOO\r\n" + 
+				"  OOOOOOOOOOOO\r\n" + 
+				"   OOOOOOOOOO\r\n");
+	}
+
+}
